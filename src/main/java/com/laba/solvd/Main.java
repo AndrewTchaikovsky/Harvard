732e00fd22daf251c2d7dcbeb41019d4b3484345
Andrey Chaykovskiy <achaykovskiy.laba@solvd.com>
2023-05-18 22:04:31 -0400

    update text

--- src/main/java/com/laba/solvd/Main.java
+++ src/main/java/com/laba/solvd/Main.java
@@ -164,5 +164,7 @@ public class Main {
         System.out.println(harvardUniversity.equals(harvardUniversity));
         System.out.println(harvardCollege.hashCode());
 
+        // ADDING FUN STUFF HERE
+
     }
 }

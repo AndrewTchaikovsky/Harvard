9ad84b84ae44de70f72a8ef1135f65877f38513d
Andrey Chaykovskiy <andy.tchakovsky@gmail.com>
2023-05-20 00:45:32 -0400

    changed main

--- src/main/java/com/laba/solvd/Main.java
+++ src/main/java/com/laba/solvd/Main.java
@@ -190,5 +190,7 @@ public class Main {
             return new University(capitalizedName, university.getLocation(), university.getFoundingYear(), university.getFaculties());
         };
 
+        // testing github
+
     }
 }
